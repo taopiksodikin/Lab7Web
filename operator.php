69a96625e3a903eeecb47d30970f00c2a2b5ce35
Taopik Sodikin <taopiksdkn08@gmail.com>
2022-11-22 01:15:33 +0700

    menambahkan file

---
+++ operator.php
@@ -0,0 +1,59 @@
+<?php
+$gaji = 1000000;
+$pajak = 0.1;
+$thp = $gaji - ($gaji*$pajak);
+    echo "Gaji sebelum pajak = Rp. $gaji <br>";
+    echo "Gaji yang dibawa pulang = Rp. $thp";
+?>
+<?php
+$nama_hari = date("l");
+if ($nama_hari == "Sunday") {
+    echo "Minggu";
+} elseif ($nama_hari == "Monday") {
+    echo "Senin";
+} else {
+    echo "Selasa";
+}
+?>
+<?php
+$nama_hari = date("l");
+switch ($nama_hari) {
+    case "Sunday":
+        echo "Minggu";
+        break;
+    case "Monday":
+        echo "Senin";
+        break;
+    case "Tuesday":
+        echo "Selasa";
+        break;
+     default:
+        echo "Sabtu";
+    }
+?>
+<?php
+echo "Perulangan 1 sampai 10 <br />";
+for ($i=1; $i<=10; $i++) {
+    echo "Perulangan ke: " . $i . '<br />';
+}
+echo "Perulangan Menurun dari 10 ke 1 <br />";
+for ($i=10; $i>=1; $i--) {
+    echo "Perulangan ke: " . $i . '<br />';
+}
+?>
+<?php
+echo "Perulangan 1 sampai 10 <br />";
+$i=1;
+while ($i<=10) {
+    echo "Perulangan ke: " . $i . '<br />';
+    $i++;
+}
+?>
+<?php
+echo "Perulangan 1 sampai 10 <br />";
+$i=1;
+do {
+    echo "Perulangan ke: " . $i . '<br />';
+    $i++;
+} while ($i<=10);
+?>
